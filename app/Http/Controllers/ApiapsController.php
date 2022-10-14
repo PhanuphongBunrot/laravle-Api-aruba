ca831e754483bffb3c816d793f2117f4c2cdcd5c
nes120658 <neszaaalou@gmail.com>
2022-10-14 16:03:42 +0700

    14/10/65

--- app/Http/Controllers/ApiapsController.php
+++ app/Http/Controllers/ApiapsController.php
@@ -11,63 +11,31 @@ class ApiapsController extends Controller
 {
     
     public function apiaps(Request $req)
-    { // "172.16.8.50", "172.16.16.50"
-
-        $conn =  new Mongo;
-        $companydb  = $conn->iparuba;
-        $tem = $companydb->temporary;
-
-
-        $mon = new Mongo;
-        $conn = $mon->iparuba->ipmaster;
-        $data = $conn->find()->toArray();
-
-
-        for ($r = 0; $r < count($data); $r++) {
-           
-            $ip = $data[$r]['ip'];
-            //echo $ip . "<br>";
-            //dd($ip);
+    { 
             try {
-                $resp = Http::timeout(5)->withHeaders([
-                    'Content-Type' => 'application/json;charset=UTF-8'
-                ])
-                    ->withOptions(["verify" => false])
-                    ->post('https://' . $ip . ':4343/rest/login', [
-                        'user' => 'admin',
-                        'passwd' => 'ssit1234'
-                    ]);
-                $sid = $resp->json()['sid'];
+                // $resp = Http::timeout(5)->withHeaders([
+                //     'Content-Type' => 'application/json;charset=UTF-8'
+                // ])
+                //     ->withOptions(["verify" => false])
+                //     ->post('https://' . $ip . ':4343/rest/login', [
+                //         'user' => 'admin',
+                //         'passwd' => 'ssit1234'
+                //     ]);
+                // $sid = $resp->json()['sid'];
                 
                 $response = Http::withHeaders([
                     'Content-Type' => 'application/json;charset=UTF-8'
                 ])
                     ->withOptions(["verify" => false])
-                    ->get('https://' . $ip . ':4343/rest/show-cmd?iap_ip_addr=' . $ip . '&cmd=show%20aps&sid=' . $sid);
+                    ->get('https://admin:admin@192.168.207.5/rest/system/resource');
+                      $data  = json_decode($response,true) ;
+                    echo ($data['build-time']);
                
-                 
-                $ex = explode('\n', $response);
-
               
-
-                for ($x = 9; $x < count($ex); $x++) {
-                    $keywords = preg_split("/[\s,]+/", $ex[$x]);
-                
-
-                   if(strlen($keywords[0]) == 1 ){
-                      $keywords[0] = null;
-                   }
-                   else if($keywords[0] != null){
-                        $keywords[0];
-                   }
-                   echo $keywords[0];
-                   
-                    
-                }
             } catch (\Illuminate\Http\Client\ConnectionException $e) {
                 //echo $ip." offline";
             }
-        }
+        
 
         
        
